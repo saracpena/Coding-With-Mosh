2bfff6272bb5e221297d90179e93cdf87e747eeb
Sara Peña <s.cristal.p@gmail.com>
2021-03-17 13:14:03 -0400

    Update index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -50,3 +50,28 @@ const people = {
 };
 
 people.talk();
+
+// ! ARRAYS : map renders lists of items
+
+const colors = ['red','green','blue'];
+//I want to render these using a list element i.e. <li>color</li>
+// I need to pass a callback function, the job of this function is to
+// transform each element in the array. This function is called by the
+// map method for each item in the colors array 'iterating'
+// it takes one item at a (time) and returns a new item {}
+//!          colors.map(function(item) {})
+        // colors.map(function(color) {
+        //     return '<li>' + color + '</li>';
+        // });
+//! THE ABOVE MAP METHOD RETURNS A NEW ARRAY MEANING IT WILL NOT MODIFY THE ORIGINAL COLORS ARRAY
+//! INSIDE A NEW ARRAY
+        // const items = colors.map(function(color){
+        //     return '<li>' + color + '</li>';
+        // });
+//!ES6 example 1
+// const items = colors.map(color => '<li>' + color + '</li>');
+//!ES6 example 2
+const items = colors.map(color => `<li>'${color}'</li>`);
+console.log(items);
+
+
