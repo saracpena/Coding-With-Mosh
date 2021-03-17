b6537cfc3daa3296353b6dd1e34fa42dd71f0999
Sara Peña <s.cristal.p@gmail.com>
2021-03-17 00:25:23 -0400

    Fat Arrow

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -11,3 +11,16 @@ person.walk();
 const walk = person.walk.bind(person);
 walk();
 
+// ES5
+const square = function(number) {
+    return number * number
+}
+// ES6
+const square1 = number => {
+    return number * number
+}
+// Shorter ES6 ONE PARAM
+           //!number goes to number * number
+const square2 = number => number * number;
+console.log(square2(5));
+
