21eba735f910f2fbb1945770165b136327d6402c
Sara Peña <s.cristal.p@gmail.com>
2021-03-17 22:57:14 -0400

    Classes

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,23 +1,33 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
-// state is a special property, an object that includes data that this Counter component needs
+  // state is a special property, an object that includes data that this Counter component needs
   state = {
-      count: 0,
+    count: 0,
+    tags: ['tag1','tag2','tag3']
   };
-  
-    render() {
+
+  render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            <li></li>
+        </ul>
       </div>
     );
   }
-//calling a function that return a value
-//this method we want to check the value of the count property 
-    formatCount(){
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
+
+  //calling a function that return a value
+  //this method we want to check the value of the count property
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
