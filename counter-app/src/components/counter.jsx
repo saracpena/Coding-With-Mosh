d5f9015152ed71ed31a5a1672c281a3e6c71a5ed
Sara Peña <s.cristal.p@gmail.com>
2021-03-21 19:04:57 -0400

    Update

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -4,25 +4,30 @@ export default class Counter extends Component {
   // state is a special property, an object that includes data that this Counter component needs
   state = {
     count: 0,
-    tags: ['tag1','tag2','tag3']
+  };
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count +1 })
   };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-            <li></li>
-        </ul>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
   //calling a function that return a value
   //this method we want to check the value of the count property
